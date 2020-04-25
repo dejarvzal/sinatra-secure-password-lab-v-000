98d86dcca35df3990483a1389a6b19d078ef5bca
Dianne Jarvis-Lumsden <diannejarvisj@gmail.com>
2020-04-25 01:48:43 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,6 +19,7 @@ class ApplicationController < Sinatra::Base
   post "/signup" do
     user = User.new(:username => params[:username], :password => params[:password])
     user.save
+    binding.pry
 		if user && user.include?(params[:username])
 			redirect "/login"
 		else
